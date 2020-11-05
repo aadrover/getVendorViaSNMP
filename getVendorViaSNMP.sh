8f16950c45d6f4819aeccf48a3233d76e05650a6
Andreu Adrover <aadrover@protonmail.com>
2020-11-05 16:32:42 +0100

    Add files via upload
    First draft

---
+++ getVendorViaSNMP.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+################################################################################
+#  This script will launch a SNMP query asking for the sysObjectID of the host
+#  Once we have it, we take onlye the vendor number of the OID
+#  Vendor examples:
+#  9 = Cisco
+#  40310 = Cumulus Network
+#  ...
+################################################################################
+
+community=$1
+host=$2
+snmpwalk -v2c -c $community $host .1.3.6.1.2.1.1.2.0 | cut -d "." -f 15
